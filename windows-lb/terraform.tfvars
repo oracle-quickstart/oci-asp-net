56aa926578fd680022ba825d62b8b262b866d959
Venkata Ramanamurthy Chandaka <venkata.ramanamurthy.chandaka@oracle.com>
2021-02-23 13:56:19 -0800

    Update ASP.net App

--- windows-lb/terraform.tfvars
+++ windows-lb/terraform.tfvars
@@ -3,8 +3,8 @@
 # If you need to update a stack, this file must be for a user in the tenancy listed in the environment file.
 
 ### Authentication details
-tenancy_ocid =  "ocid1.tenancy.oc1..aaaaaaaawgbzspd6cjovpmic7dahwbalg4opcbixgguo6z2s7t335m6lalbq" #"ocid1.tenancy.oc1..aaaaaaaagkbzgg6lpzrf47xzy4rjoxg4de6ncfiq2rncmjiujvy2hjgxvziq"
-user_ocid = "ocid1.user.oc1..aaaaaaaakbm65mctre4i4wom6sam6z4amtjc5vin3iqsa4z3bzpdvoaieioq" #"ocid1.user.oc1..aaaaaaaaju4h4lir5jpigeze4u5u53s2sz3qye3xcrgr7mczvbgzprmzme3q"
+tenancy_ocid =  "ocid1.tenancy.oc1..aaaaaaaawgbzspd6cjovpmic7dahwbalg4opcbixgguo6z2s7t335m6lalbq" 
+user_ocid = "ocid1.user.oc1..aaaaaaaakbm65mctre4i4wom6sam6z4amtjc5vin3iqsa4z3bzpdvoaieioq" 
 fingerprint = "04:25:17:8c:0c:e7:f0:9b:c6:14:c2:15:06:e7:66:c1"
 private_key_path = "/home/runner/.oci/key.pem"
 
@@ -12,4 +12,4 @@ private_key_path = "/home/runner/.oci/key.pem"
 region = "us-phoenix-1"
 
 ### Compartment
-compartment_ocid = "ocid1.compartment.oc1..aaaaaaaagxh5kocy6g6of5znwe35ckxn6ddkdqwoif7qjcx3igm2fareke2q" #"ocid1.compartment.oc1..aaaaaaaahfbspptezfjw34xm2kxpyo3i6qcc6zhlyguglt3idwmsqnqd5ddq"
+compartment_ocid = "ocid1.compartment.oc1..aaaaaaaagxh5kocy6g6of5znwe35ckxn6ddkdqwoif7qjcx3igm2fareke2q"
--- windows-lb/userdata/deployapp.ps1
+++ windows-lb/userdata/deployapp.ps1
@@ -1,11 +1,5 @@
-
-
-echo 'Hello from terraform. Creating a simple website' > C:\hello_from_terraform_1234567890.txt
+ 
 Start-Process powershell -Verb runAs
-
-# Unzip the project
-# Unzip the project
-mkdir C:\Project
 Add-Type -AssemblyName System.IO.Compression.FileSystem
 function Unzip
 {
@@ -13,15 +7,32 @@ function Unzip
     [System.IO.Compression.ZipFile]::ExtractToDirectory($zipfile, $outpath)
 }
 
+$redeploy = $false
+# Remove Application directory if it was previously deployed in previous attempt
+If(test-path "C:\Application")
+{
+   $redeploy = $true;
+   iisreset /stop
+   Remove-Item -LiteralPath "C:\Application" -Force -Recurse
+   Remove-Item -LiteralPath "C:\Project" -Force -Recurse
+}
+
+New-Item -ItemType Directory -Force -Path "C:\Application"
 Unzip "C:\Temp\App.zip" "C:\Application"
+# Create Project Directory to drop ASP.net files
+New-Item -ItemType Directory -Force -Path "C:\Project"
 Move-Item -Path C:\Application\mvctest\* -Destination C:\Project
 
 # Install windows features
-Install-WindowsFeature -Name Web-Server -IncludeAllSubFeature -IncludeManagementTools
-Import-Module WebAdministration
-Get-WebSite -Name "Default Web Site" | Remove-WebSite -Confirm:$false -Verbose
-New-Website -Name venkatatest -ApplicationPool DefaultAppPool -IPAddress * -PhysicalPath C:\Project -Port 80
-iisreset
+If($redeploy -eq $false) {
+   Install-WindowsFeature -Name Web-Server -IncludeAllSubFeature -IncludeManagementTools
+   Import-Module WebAdministration
+   Get-WebSite -Name "Default Web Site" | Remove-WebSite -Confirm:$false -Verbose
+   New-Website -Name "ASPNETTEST" -ApplicationPool DefaultAppPool -IPAddress * -PhysicalPath C:\Project -Port 80
+} else {
+   Get-WebSite -Name "ASPNETTEST" | Remove-WebSite -Confirm:$false -Verbose
+   New-Website -Name "ASPNETTEST" -ApplicationPool DefaultAppPool -IPAddress * -PhysicalPath C:\Project -Port 80
+}
 
 # Quick way to download the Windows Hosting Bundle installers which are
 # then be executed on the VM ...
@@ -37,25 +48,38 @@ function Perform-Download-Install {
    }
 
    $whb_installer_file = $temp_path + [System.IO.Path]::GetFileName($installUrl)
-
    Try
    {
-      Invoke-WebRequest -Uri $installUrl -OutFile $whb_installer_file
-      Write-Output "Installer downloaded"
+      $reinstall = $true
+      If(!(test-path $whb_installer_file))
+      {
+         $reinstall = $false
+         Invoke-WebRequest -Uri $installUrl -OutFile $whb_installer_file
+         Write-Output "Installer downloaded"
+      }
+
       Write-Output "- Execute the $whb_installer_file to install the ASP.Net Core Runtime"
       $args = New-Object -TypeName System.Collections.Generic.List[System.String]
       $args.Add("/quiet")
-      $args.Add("/install")
+
+      # If its reinstall repair the app else freshly install
+      If($reinstall -eq $true) {
+         $args.Add("/repair")
+      }
+      else {
+         $args.Add("/install")
+      }
+
       $args.Add("/norestart")
       $Output = Start-Process -FilePath $whb_installer_file -ArgumentList $args -NoNewWindow -Wait -PassThru
       If($Output.Exitcode -eq 0)
       {
-        Write-Output "Successfully installed"
+         Write-Output "Successfully installed"
       }
       else
       {
-        Write-Error "`t`t Something went wrong with the installation. Errorlevel: ${Output.ExitCode}"
-        Exit 1
+         Write-Error "`t`t Something went wrong with the installation. Errorlevel: ${Output.ExitCode}"
+         Exit 1
       }
    }
    Catch
@@ -67,6 +91,5 @@ function Perform-Download-Install {
 
 ## Perform-Download-Install -installUrl "https://download.visualstudio.microsoft.com/download/pr/75483251-b77a-41a9-9ea2-05fb1668e148/2c27ea12ec2c93434c447f4009f2c2d2/dotnet-sdk-5.0.102-win-x64.exe"
 Perform-Download-Install -installUrl "https://download.visualstudio.microsoft.com/download/pr/43ea9d7a-39c9-467a-83e6-548b3faf832d/5e7d573d9c4f40d0c1192aa2319f07c5/dotnet-hosting-5.0.2-win.exe"
-
-
-
+iisreset /stop
+iisreset /start
