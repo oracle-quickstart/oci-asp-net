1305b409466bce37152866e1a51a0bd71b1a360f
Jeevan Joseph <jeevan.g@gmail.com>
2021-03-26 00:35:16 -0700

    cleaning up outputs

--- windows-lb/output.tf
+++ windows-lb/output.tf
@@ -16,15 +16,7 @@ output "instance_public_ip2" {
   value = [oci_core_instance.instance2.public_ip]
 }
 
-output "instance_private_ip1" {
-  value = [oci_core_instance.instance1.private_ip]
-}
-
-output "instance_private_ip2" {
-  value = [oci_core_instance.instance2.private_ip]
-}
-
 output "lb_public_ip" {
-  value = [oci_load_balancer.lb1.ip_address_details]
+  value = oci_load_balancer.lb1.ip_address_details[0].ip_address
 }
 
