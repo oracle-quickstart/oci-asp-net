47d2c49855f726488e8a08131042aa011355b4e6
Lukasz Feldman <lukasz.feldman@oracle.com>
2021-07-15 11:15:21 +0200

    Added tagging, optional db tier

--- windows-lb/variables.tf
+++ windows-lb/variables.tf
@@ -1,3 +1,5 @@
+## Copyright © 2021, Oracle and/or its affiliates. 
+## All rights reserved. The Universal Permissive License (UPL), Version 1.0 as shown at http://oss.oracle.com/licenses/upl
 
 # Authentcation variables to be commented when using ORM
 variable "user_ocid" {}
@@ -8,37 +10,46 @@ variable "region" {}
 variable "tenancy_ocid" {}
 variable "compartment_ocid" {}
 
-
-variable "load_balancer_shape_details_maximum_bandwidth_in_mbps" {
-  default = 100
+variable "availablity_domain_name" {
+  default = ""
 }
 
-variable "load_balancer_shape_details_minimum_bandwidth_in_mbps" {
-  default = 10
+variable "deploy_db_tier" {
+  default = true
+}
+variable "instance_password" {}
+
+variable "release" {
+  description = "Reference Architecture Release (OCI Architecture Center)"
+  default     = "1.0"
+}
+
+variable "lb_shape" {
+  default = "flexible"
+}
+
+variable "flex_lb_min_shape" {
+  default = "10"
+}
+
+variable "flex_lb_max_shape" {
+  default = "100"
 }
 
 variable "instance_shape" {
   default = "VM.Standard2.1"
 }
 
+variable "shape_flex_ocpus" {
+  default = 1
+}
+
+variable "shape_flex_memory" {
+  default = 10
+}
+
 variable "instance_name" {
   default = "TFWindows"
-}
-
-variable "availability_domain" {
-  default = 1
-}
-
-variable "num_nodes" {
-  default = 2
-}
-
-variable "instance_ocpus" {
-  default = 1
-}
-
-variable "instance_password" {
-
 }
 
 variable "userdata" {
@@ -64,20 +75,38 @@ variable "image_os_version" {
   default = "Server 2012 R2 Standard"
 }
 
-variable "subnet_cidr" {
-  type    = list(string)
-  default = ["10.1.20.0/24", "10.1.21.0/24"]
+variable "vcn_cidr" {
+  default = "10.1.0.0/16"
 }
 
+variable "subnet1_cidr" {
+  default = "10.1.20.0/24"
+}
+
+variable "subnet2_cidr" {
+  default = "10.1.21.0/24"
+}
+
+variable "mssql_primary_shape" {
+  default = "VM.Standard2.2"
+}
+
+variable "mssql_standby_shape" {
+  default = "VM.Standard2.2"
+}
+
+# Dictionary Locals
 locals {
   compute_flexible_shapes = [
     "VM.Standard.E3.Flex",
-    "VM.Standard.E4.Flex"
+    "VM.Standard.E4.Flex",
+    "VM.Optimized3.Flex",
+    "VM.Standard.A1.Flex"
   ]
-  compute_shape_flexible_descriptions = [
-    "Cores for Standard.E3.Flex and BM.Standard.E3.128 Instances",
-    "Cores for Standard.E4.Flex and BM.Standard.E4.128 Instances"
-  ]
-  compute_shape_flexible_vs_descriptions = zipmap(local.compute_flexible_shapes, local.compute_shape_flexible_descriptions)
-  compute_shape_description              = lookup(local.compute_shape_flexible_vs_descriptions, local.instance_shape, local.instance_shape)
+}
+
+# Checks if is using Flexible Compute Shapes
+locals {
+  is_flexible_node_shape = contains(local.compute_flexible_shapes, var.instance_shape)
+  is_flexible_lb_shape   = var.lb_shape == "flexible" ? true : false
 }
